93bdbb7c6e5956d9f4d4c6c7bd2a8823feb3740c
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-02 08:15:44 -0300

    Remove contact from meeting store

--- derbel-app/src/stores/meeting.store.ts
+++ derbel-app/src/stores/meeting.store.ts
@@ -4,6 +4,5 @@ import { ref } from 'vue';
 
 export const useMeetingStore = defineStore('meeting', () => {
   const meeting = ref<IMeeting | null>();
-  const contact = ref<IMeeting | null>();
-  return { meeting, contact };
+  return { meeting };
 });
